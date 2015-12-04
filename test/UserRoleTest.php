6773483ec8d39e4995a1942d6e5db50890f12a41
ganesh10git <tosgk08@gmail.com>
2015-12-04 07:27:53 +0530

    Sample unit test case for user role

--- test/UserRoleTest.php
+++ test/UserRoleTest.php
@@ -11,7 +11,7 @@ require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 class UserTest extends PHPUnit_Framework_TestCase {
-  public function test_search_for_titles() {
+  public function test_user_roles() {
     $input_roles_array = array('input' => array('selected_role' => array('76' => array('9-expert' => '1', '3-content_manager_action' => '1'))));
     $check_user_roles = im_user_dashboard_process_action($input_roles_array, 'add');    
   }
--- test/UserTest.php
+++ test/UserTest.php
@@ -11,7 +11,7 @@ require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 class UserTest extends PHPUnit_Framework_TestCase {
-  public function test_search_for_titles() {
+  public function test_region_code() {
     $titles = fetch_region_code('3021080655604');
     $this->assertEquals($titles, 'TEST Store A');
   }
