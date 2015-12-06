ac1caaa701e01e73f19fcdc88576779d37a160de
ganesh10git <tosgk08@gmail.com>
2015-12-06 21:43:56 +0530

    phpunit breakpoint using xdebug

--- test/UserRoleTest.php
+++ test/UserRoleTest.php
@@ -10,10 +10,14 @@ define('DRUPAL_ROOT', getcwd());
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+module_load_include('inc', 'im_user', 'includes/im_user_dashboard');
+
 class UserTest extends PHPUnit_Framework_TestCase {
   public function test_user_roles() {
-    $input_roles_array = array('input' => array('selected_role' => array('76' => array('9-expert' => '1', '3-content_manager_action' => '1'))));
-    $check_user_roles = im_user_dashboard_process_action($input_roles_array, 'add');    
+    $_SESSION["dashboard_regional_moderator_users"]['76']['profile_nids'] = '1267';
+    $input_roles_array = array('input' => array('selected_role' => array('76' => array('9-expert' => '1', '3-content_manager_action' => '1', '17-content_manager_action_regional' => '1'))), 'values' => array('users_roles_details' => array('76' => '76')));
+    $check_user_roles = im_user_dashboard_process_action($input_roles_array, 'remove');
+    xdebug_break();
   }
 }
 ?>
