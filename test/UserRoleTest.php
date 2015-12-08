ae698f2cb78084f470a29ffb5e82811a70a35e38
ganesh10git <tosgk08@gmail.com>
2015-12-08 07:32:15 +0530

    Removed the Xdebug_break

--- test/UserRoleTest.php
+++ test/UserRoleTest.php
@@ -17,8 +17,7 @@ class UserRole extends PHPUnit_Framework_TestCase {
   public function test_user_roles() {
     $_SESSION["dashboard_regional_moderator_users"]['76']['profile_nids'] = '1267';
     $input_roles_array = array('input' => array('selected_role' => array('76' => array('9-expert' => '1', '3-content_manager_action' => '1', '17-content_manager_action_regional' => '1'))), 'values' => array('users_roles_details' => array('76' => '76')));    
-    $check_user_roles = im_user_dashboard_process_action($input_roles_array, 'add');
-    xdebug_break();    
+    $check_user_roles = im_user_dashboard_process_action($input_roles_array, 'add');   
   }
 }
 ?>
